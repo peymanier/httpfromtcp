299cb8230a5ff86bfc1bc024940fcd1865c2d92a
Peyman <hassani.peyman627@gmail.com>
2025-08-14 14:26:03 +0330

    Add http request line parsing

---
+++ internal/request/request.go
@@ -0,0 +1,65 @@
+package request
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+var ErrMalformedRequestLine = fmt.Errorf("malformed request-line")
+var ErrUnsupportedHTTPVersion = fmt.Errorf("unsupported http version")
+var SEPARATOR = "\r\n"
+
+func parseRequestLine(b string) (*RequestLine, string, error) {
+	idx := strings.Index(b, SEPARATOR)
+	if idx == -1 {
+		return nil, b, nil
+	}
+
+	startLine := b[:idx]
+	restOfMsg := b[idx+len(SEPARATOR):]
+
+	parts := strings.Split(startLine, " ")
+	if len(parts) != 3 {
+		return nil, restOfMsg, ErrMalformedRequestLine
+	}
+
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, restOfMsg, ErrMalformedRequestLine
+	}
+
+	rl := &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HttpVersion:   httpParts[1],
+	}
+
+	return rl, restOfMsg, nil
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, errors.Join(fmt.Errorf("unable to read"), err)
+	}
+
+	str := string(data)
+	rl, _, err := parseRequestLine(str)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{RequestLine: *rl}, nil
+}
