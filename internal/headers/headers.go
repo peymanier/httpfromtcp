7897295e3a65ad500ddfffeb02e4bc5634363438
Peyman <hassani.peyman627@gmail.com>
2025-10-16 14:55:50 +0330

    Update valid token check for parsing headers

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -6,6 +6,24 @@ import (
 	"strings"
 )
 
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if (ch > 'A' && ch < 'Z') || (ch > 'a' && ch < 'z') || (ch >= '0' && ch <= '9') {
+			found = true
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '\\', '*', '+', '-', '.', '^', '_', '`', '|':
+			found = true
+		}
+
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
 func parseHeader(fieldLine []byte) (string, string, error) {
 	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
 	if len(parts) != 2 {
@@ -59,6 +77,10 @@ func (h *Headers) Parse(data []byte) (int, bool, error) {
 		name, value, err := parseHeader(data[read : read+idx])
 		if err != nil {
 			return 0, false, err
+		}
+
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed header name")
 		}
 
 		read += idx + len(rn)
